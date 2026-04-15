887e783fec39ddd8a69685544519ae6d92406b10
Panpan <2398577209@qq.com>
2026-04-15 20:38:34 +0800

    人才档案操作

--- src/main/java/com/rehab/managerv2/controller/SysTalentProfileController.java
+++ src/main/java/com/rehab/managerv2/controller/SysTalentProfileController.java
@@ -1,40 +1,52 @@
 package com.rehab.managerv2.controller;
 
-
-import com.rehab.managerv2.common.Result;
 import com.rehab.managerv2.entity.SysTalentProfile;
 import com.rehab.managerv2.service.SysTalentProfileService;
+import com.rehab.managerv2.common.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * <p>
- * 交叉人才基本档案表 前端控制器
- * </p>
- *
- * @author author
- * @since 2026-04-15
- */
 @RestController
 @RequestMapping("/sys-talent-profile")
 public class SysTalentProfileController {
 
     @Autowired
-    private SysTalentProfileService SystalentProfileService;
+    private SysTalentProfileService sysTalentProfileService;
 
     /**
-     * 获取所有交叉人才档案列表
+     * 获取所有人才档案列表
      */
     @GetMapping("/list")
     public Result getList() {
-        // 使用 MyBatis-Plus 自带的 list() 方法查询所有数据
-        List<SysTalentProfile> list = SystalentProfileService.list();
+        List<SysTalentProfile> list = sysTalentProfileService.list();
         return Result.success(list);
     }
 
+    /**
+     * 保存或更新人才档案
+     */
+    @PostMapping("/save")
+    public Result save(@RequestBody SysTalentProfile sysTalentProfile) {
+        boolean success = sysTalentProfileService.saveOrUpdate(sysTalentProfile);
+        if (success) {
+            return Result.success("保存成功");
+        }
+        // 🔥 修改点：加上了 500 错误码，完美匹配你的 Result 类！
+        return Result.error(500, "保存失败");
+    }
+
+    /**
+     * 删除人才档案
+     */
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Long id) {
+        boolean success = sysTalentProfileService.removeById(id);
+        if (success) {
+            return Result.success("删除成功");
+        }
+        // 🔥 修改点：加上了 500 错误码！
+        return Result.error(500, "删除失败");
+    }
 }
